ba2f37c9d1f166468290c243d5760a52f767e9cc
fk219 <furqank219@gmail.com>
2026-01-28 18:19:30 +0400

    feat: add a new BentoGrid component with animated cards and a responsive layout.

--- components/Home/BentoGrid.tsx
+++ components/Home/BentoGrid.tsx
@@ -61,7 +61,7 @@ const BentoGrid: React.FC = () => {
     }, []);
 
     return (
-        <section className="py-24 md:py-32 px-4 md:px-8 lg:px-12 bg-[#EAE8E3] dark:bg-[#0A0908]">
+        <section className="py-24 md:py-32 px-4 md:px-8 lg:px-12 bg-[#F9F8F4] dark:bg-[#0A0A0B]">
             <div ref={containerRef} className="max-w-[1600px] mx-auto">
                 {/* Section Header */}
                 <div className="flex flex-col md:flex-row justify-between items-start md:items-end mb-12 md:mb-16">
